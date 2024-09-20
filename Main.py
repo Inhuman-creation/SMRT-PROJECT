979a3fb40dc3cf7d3ceec7137255594463bc4656
Inhuman-creation <stradgn@gmail.com>
2024-09-19 20:35:00 -0500

    Code Performance Fixes
    Removed redundant files, Fixed GUI errors.

--- Main.py
+++ Main.py
@@ -1,7 +1,12 @@
-import ChoiceGUI
-import MenuGUI
-from ChoiceGUI import MultipleChoiceGUI
-from MenuGUI import Menu
+#import ChoiceGUI
+#import MenuGUI
+#from ChoiceGUI import MultipleChoiceGUI
+#from MenuGUI import Menu
+import customtkinter as ctk
+from GUIClass import GUI
 
-object1 = Menu()
+#object1 = Menu()
 #MultipleChoiceGUI()
+
+object1=GUI()
+object1.menuGUI()
