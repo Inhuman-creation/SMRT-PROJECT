eb4e1f3df7a481aa83dd78a7fe0dc8b599464833
Jacob Poland <45803275+JacobDPoland@users.noreply.github.com>
2024-09-23 18:29:39 -0500

    Update Main.py
    Removed calling of menuGUI() in Main.py because it is now in __init__() in menuGUI.
    
    Removed extra comments

--- Main.py
+++ Main.py
@@ -1,12 +1,3 @@
-#import ChoiceGUI
-#import MenuGUI
-#from ChoiceGUI import MultipleChoiceGUI
-#from MenuGUI import Menu
-import customtkinter as ctk
 from GUIClass import GUI
 
-#object1 = Menu()
-#MultipleChoiceGUI()
-
 object1=GUI()
-object1.menuGUI()
