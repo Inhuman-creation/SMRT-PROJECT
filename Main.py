dc764f0bbd1cc45285398a0d5ee25ba38fbc0154
Inhuman-creation <stradgn@gmail.com>
2024-10-01 09:39:22 -0500

    Removal of test code
    .

--- Main.py
+++ Main.py
@@ -4,12 +4,3 @@ from Word import Word
 
 object1=GUI()
 
-#testing walking window and word classes
-study_window = WalkingWindow(size=10)
-study_window.read_from_csv("Spanish.csv", num_rows=10)
-random_words = study_window.get_random_words(5)
-print(random_words)
-
-#test checking definition
-test_word = Word(english = 'test', spanish = 'spanishtest')
-print(f"Test definition works? {test_word.check_definition('test')}")
