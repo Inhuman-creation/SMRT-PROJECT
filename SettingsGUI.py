54c32be0e2c0e40c5533c6b19e5e2254421e4df2
Vangsvatnet <133836194+Vangsvatnet@users.noreply.github.com>
2024-11-12 15:24:09 -0600

    added (in progress) to french option

--- SettingsGUI.py
+++ SettingsGUI.py
@@ -112,7 +112,7 @@ class SettingsGUI:
         #dropdown menu for language selection
         ctk.CTkLabel(self.frame, text="Language Selection", font=labelfont,
                      text_color="black").place(relx=0.6, rely=slider_start_y, anchor=tk.CENTER)
-        options = ["Spanish", "French"]
+        options = ["Spanish", "French (in-progress)"]
         self.language_var = tk.StringVar(value=Settings.LANGUAGE)
         self.language_dropdown = ctk.CTkOptionMenu(self.app, values=options, variable=self.language_var,
                                                    font=labelfont, text_color="white",
